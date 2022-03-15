933c20919d55b090a697ceb692cb2b2fe87c1de1
Mateusz Suchocki <mateuszsuchocki7@gmail.com>
2022-03-15 17:01:52 -0400

    Simple steghide extract loop with wordlist

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ In main script there will be implemented multithreading and option parser for di
 """
 import argparse
 
+from src.cracker import crack_password
+
 
 def print_hi(name):
     print(f'Hi, {name}')
@@ -11,3 +13,5 @@ def print_hi(name):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="StegoCracker - easy way to crack steganography")
     args = parser.parse_args()
+
+    crack_password('../resources/dog.jpg', '../resources/common.txt')
