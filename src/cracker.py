bf2b1a975e85fa805db7ad84f6fa619f88673f9f
Mateusz Suchocki <mateuszsuchocki7@gmail.com>
2022-03-16 11:56:06 -0400

    Add CLI options to a script

--- src/cracker.py
+++ src/cracker.py
@@ -4,15 +4,16 @@ This file will contain the main brute force loop. Each cracking method will be i
 from subprocess import DEVNULL, run
 
 
-def crack_password(file, wordlist):
+def crack_password(file, wordlist, output):
     with open(wordlist, 'r') as wl:
         for password in wl:
-            print('Trying passphrase:', password.strip())
-            p = run(['steghide', 'extract', '-sf', file, '-xf', '../resources/out', '-p', password.strip(), '-f'],
+            # print('Trying passphrase:', password.strip())
+            p = run(['steghide', 'extract', '-sf', file, '-xf', output, '-p', password.strip(), '-f'],
                     stderr=DEVNULL)
 
             if p.returncode == 0:
                 print('Found passphrase:', password)
+                print('Output is in file:', output)
                 break
 
 
--- src/main.py
+++ src/main.py
@@ -2,16 +2,53 @@
 In main script there will be implemented multithreading and option parser for different crack methods
 """
 import argparse
+import os
+from distutils.spawn import find_executable
 
-from src.cracker import crack_password
+from cracker import crack_password
 
 
-def print_hi(name):
-    print(f'Hi, {name}')
+def main():
+    parser = argparse.ArgumentParser(description="StegoCracker - easy way to crack steganography")
+
+    parser.add_argument('file', help='A file to crack')
+    parser.add_argument('-m', '--method', help='Cracking method - "password", "lsb", "html"')
+    parser.add_argument('-w', '--wordlist', default=None, help='A wordlist file to be used in cracking')
+    parser.add_argument('-o', '--output', default=None, help='A output file for cracking')
+
+    args = parser.parse_args()
+
+    file_path = args.file
+    method = args.method
+    wordlist_path = args.wordlist
+    output = args.output or file_path + '.out'
+
+    if not os.path.isfile(file_path):
+        print(f'File {file_path} does not exist!')
+        exit()
+
+    if method is None:
+        print('You must specify a cracking method!')
+        exit()
+
+    if not os.path.isfile(wordlist_path):
+        print(f'Wordlist {wordlist_path} does not exist!')
+        exit()
+
+    if not find_executable('steghide'):
+        print('"steghide" is not installed. Run "sudo apt install steghide -y to install"')
+        exit()
+
+    if method == 'password':
+        print('Cracking file with password method')
+        crack_password(file_path, wordlist_path, output)
+    elif method == 'lsb':
+        print('Cracking file with lsb method')
+    elif method == 'html':
+        print('Cracking file with html method')
+    else:
+        print('Wrong cracking method specified. Possible methods - "password", "lsb", "html"')
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="StegoCracker - easy way to crack steganography")
-    args = parser.parse_args()
-
-    crack_password('../resources/dog.jpg', '../resources/common.txt')
+    main()
